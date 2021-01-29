7dd0dc7932eebef4a4a872202bcd7728abc7234b
diodonfrost <diodon.frost@diodonfrost.me>
2021-01-29 17:50:12 +0100

    refactor(url_parsing): put url github parsing in a class
    This commit create a Python class to parse and extract
    information on a given Github url without authentification.

--- generate.py
+++ generate.py
@@ -8,25 +8,79 @@ from github import Github
 import jinja2
 
 
-def generate_markdown_badge(workflow_name, workflow_url, workflow_badge_url) -> str:
-    """Generate Github-Actions badge url in markdown format.
+class GithubUrlParsing:
+    """Extract information from Github url."""
 
-    :param str workflow_name:
-        Github-Actions workflow name.
-    :param str workflow_badge_url:
-        Github-Actions badge url.
-    :param str workflow_url:
-        Github-Actions url.
+    def __init__(self, repo_full_name, github_base_url="https://github.com"):
+        """Initialize GithubUrlParsing class.
 
-    return: str markdown_badge_url:
-        The fully Github-Actions badge url in markdown format.
+        :param str repo_full_name:
+            Github user/organization + repo name, example: python/mypy
+        :param str github_base_url:
+            Default Github url to used, can be a Github Enterprise url
+        """
+        self.github_repo_url = github_base_url + "/" + repo_full_name
 
-    >>> generate_markdown_actions_badge("pytest", "https://github.com/pytest-dev/pytest/workflows/main/badge.svg", "https://github.com/pytest-dev/pytest/actions")
-    '[![pytest](https://github.com/pytest-dev/pytest/workflows/main/badge.svg)](https://github.com/pytest-dev/pytest/actions)'
-    """
-    return (
-        "[![" + workflow_name + "](" + workflow_badge_url + ")](" + workflow_url + ")"
-    )
+    def get_workflow_url(self):
+        """Get Github-Actions workflow url.
+
+        :return str github_workflow_url:
+            The fully Github-Action url
+
+        >>> t = GithubUrlParsing(repo_full_name="python/mypy")
+        >>> t.get_workflow_url()
+        'https://github.com/python/mypy/actions'
+        """
+        return self.github_repo_url + "/actions"
+
+    def get_badge_url(self, workflow_name):
+        """Get github badge url for the given repository.
+
+        :param str workflow_name:
+            The Github-Action workflow name.
+
+        :return str github_badge_url_status:
+            The fully Github-Action badge status url.
+
+        >>> t = GithubUrlParsing(repo_full_name="python/mypy")
+        >>> t.get_badge_url(workflow_name="main")
+        'https://github.com/python/mypy/workflows/main/badge.svg'
+
+        >>> t.get_badge_url(workflow_name="Run mypy_primer")
+        'https://github.com/python/mypy/workflows/Run%20mypy_primer/badge.svg'
+        """
+        return (
+            self.github_repo_url
+            + "/workflows/"
+            + workflow_name.replace(" ", "%20")
+            + "/badge.svg"
+        )
+
+    def get_markdown_badge_url(self, workflow_name):
+        """Get Github badge url in markdown format.
+
+        :param str workflow_name:
+            The Github-Action workflow name.
+
+        :return str github_badge_url_status:
+            The fully Github-Action badge status url in markdown format.
+
+        >>> t = GithubUrlParsing(repo_full_name="python/mypy")
+        >>> t.get_markdown_badge_url(workflow_name="main")
+        '[![main](https://github.com/python/mypy/workflows/main/badge.svg)](https://github.com/python/mypy/actions)'
+
+        >>> t.get_markdown_badge_url(workflow_name="Run mypy_primer")
+        '[![Run mypy_primer](https://github.com/python/mypy/workflows/Run%20mypy_primer/badge.svg)](https://github.com/python/mypy/actions)'
+        """
+        return (
+            "[!["
+            + workflow_name
+            + "]("
+            + self.get_badge_url(workflow_name)
+            + ")]("
+            + self.get_workflow_url()
+            + ")"
+        )
 
 
 def generate_file(template_file, template_vars, template_dir_path="templates") -> None:
@@ -57,17 +111,16 @@ def main():
         workflow_map = {"repo_name": repo_info.name, "markdown_badge_urls": []}
 
         for workflow in repo_info.get_workflows():
-            github_workflow_markdown_badge_url = generate_markdown_badge(
-                workflow_name=workflow.name,
-                workflow_badge_url=workflow.badge_url,
-                workflow_url=repo_info.html_url + "/actions",
-            )
-            workflow_map["markdown_badge_urls"].append(github_workflow_markdown_badge_url)
+            parse = GithubUrlParsing(repo_full_name=repo_info.full_name)
+            markdown_badge = parse.get_markdown_badge_url(workflow_name=workflow.name)
+            workflow_map["markdown_badge_urls"].append(markdown_badge)
+
         if workflow_map["markdown_badge_urls"]:
             github_workflows.append(workflow_map)
-    
+
     generate_file(
-        template_file="README.md.j2", template_vars={"github_workflows": github_workflows}
+        template_file="README.md.j2",
+        template_vars={"github_workflows": github_workflows},
     )
 
 
